66010fc63882c8b62718acbc44d6a75f81558acc
Daniel <roderm99@gmail.com>
2021-05-30 11:15:01 -0400

    Press and hold to draw function, rainbow coloring

--- etchScript.js
+++ etchScript.js
@@ -7,9 +7,9 @@ cellDiv.style.border = '1px solid';
 cellDiv.style.borderColor = 'black';
 cellDiv.classList.add('cellDivClass');
 
-//clear (reset) button functionality
-const btn = document.querySelector('#clear-btn');
-btn.addEventListener('click', () => {
+//Reset button functionality
+const resizeBtn = document.querySelector('#resize-btn');
+resizeBtn.addEventListener('click', () => {
     let sizeInput;
     do {
         sizeInput = prompt("Enter a number between 1 and 100 for grid dimensions i.e. Enter 32 for a 32x32 grid");
@@ -19,7 +19,18 @@ btn.addEventListener('click', () => {
     } while (sizeInput > 100 && sizeInput);
 });
 
-//Variables for calculating individual cell size
+//Clear button 
+const resetBtn = document.querySelector('#reset-btn');
+resetBtn.addEventListener('click', resetPad);
+
+function resetPad(){
+    genGrid(16);
+}
+
+
+
+
+//Variables for calculating total grid area - used for calculating individual cell size
 let skContWidth = document.getElementById('sketch-grid-container').clientWidth;
 let skContHeight = document.getElementById('sketch-grid-container').clientHeight;
 let totalArea = Number(skContWidth) * Number(skContHeight);
@@ -30,15 +41,31 @@ let cellW = Math.sqrt(totalArea/size);
 let cellH = Math.sqrt(totalArea/size);
 let cellDivCopy = cellDiv.cloneNode(true);
 
+/*let r = Math.floor(Math.random() * 256);
+let g = Math.floor(Math.random() * 256);
+let b = Math.floor(Math.random() * 256);*/
+
+let isDrawing = false;
+
 cellDiv.style.width = `${cellW - 2}px`;
 cellDiv.style.height = `${cellH - 2}px`;
 
 for (i = 0; i < size; i++){
     cellDivCopy = cellDiv.cloneNode(true);
-    cellDivCopy.addEventListener('mouseenter', (e) => {
-        console.log(e.target);
-        e.target.style.backgroundColor = 'purple';
+    cellDivCopy.addEventListener('mousedown', (e) => {
+        //e.target.style.backgroundColor = 'purple';
+        e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
+        isDrawing = true;
     });    
+    cellDivCopy.addEventListener('mouseup', () => {
+        isDrawing = false;
+    });
+    cellDivCopy.addEventListener('mouseenter', (e) => {
+        if (isDrawing === true) {
+            //e.target.style.backgroundColor = 'purple';
+            e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
+        }
+    })  
     skContainer.appendChild(cellDivCopy);
 }
 
@@ -49,20 +76,48 @@ function genGrid(n){
     size = n * n;
     let cellW2 = Math.sqrt(totalArea/size);
     let cellH2 = Math.sqrt(totalArea/size);
+    
 
     cellDiv.style.width = `${cellW2 - 2}px`;
     cellDiv.style.height = `${cellH2 - 2}px`;
 
+    
+
     for (i = 0; i < size; i++){
         cellDivCopy = cellDiv.cloneNode(true);
-        cellDivCopy.addEventListener('mouseenter', (e) => {
-            console.log(e.target);
-            e.target.style.backgroundColor = 'purple';
+        cellDivCopy.addEventListener('mousedown', (e) => {
+            //e.target.style.backgroundColor = 'purple';
+            e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
+            isDrawing = true;
         });    
+        cellDivCopy.addEventListener('mouseup', () => {
+            isDrawing = false;
+        });
+        cellDivCopy.addEventListener('mouseenter', (e) => {
+            if (isDrawing === true) {
+                //e.target.style.backgroundColor = 'purple';
+                e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
+            }
+        })
         skContainer.appendChild(cellDivCopy);
     }
 
 }
+
+function genRandomRgb(max){
+    Math.floor(math.random() * max);
+}
+
+
+/*
+//Green Button
+const gBtn = document.querySelector('#green-btn');
+gBtn.addEventListener('click');
+//Red Button
+
+function drawGreen(e){
+    e.target.style.backgroundColor = 'green';
+}*/
 
 //Trying to recreate removeAllChildren function with for loop
 function clearGrid() {
